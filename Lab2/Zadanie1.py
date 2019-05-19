9178d04bc947d2f7431a6c2b03636b83dce4908f
Irina Kuranova <47595007+cryuso@users.noreply.github.com>
2019-05-19 20:58:35 +0300

    Update Zadanie1.py

--- Lab2/Zadanie1.py
+++ Lab2/Zadanie1.py
@@ -1,5 +1,6 @@
-string = 'дров, псов, план, курс, слов'
+string = 'слов, пробелов, измениний, знаков, кошечка'
 string = string.split(', ') #разбивает строку на части и возвращает в виде списка
 for i in range(0, len(string)):
-    if string[i][2] == 'о' and string[i][3] == 'в':
+    if string[i][len(string[i])-1] == "в" and string[i][len(string[i])-2] == "о":
+    #if string[i][-1] == "в" and string[i][-2] == "о":
         print(string[i])
